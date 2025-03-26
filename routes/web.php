a76c5c2e6b7ce35b2116221e6fbf90607968adb2
Frrama <arjunafadli@gmail.com>
2025-03-26 23:05:14 +0700

    update roles, penambahan Dasboard Controller

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::middleware(['auth'])->get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
