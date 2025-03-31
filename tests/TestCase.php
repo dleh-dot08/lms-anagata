89960139f2ccc15d88381a64d241ad43c336df34
Oktavianus Hendry <oktavianus.hendry@student.pradita.ac.id>
2025-03-31 14:07:53 +0700

    test workflows

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,18 @@
 
 namespace Tests;
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    use CreatesApplication, RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // Ensure roles are seeded before each test
+        $this->seed(\Database\Seeders\RoleSeeder::class);
+    }
 }
