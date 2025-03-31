2e83eb8fabe99e634bce0052295a62f9477afabf
Oktavianus Hendry <oktavianus.hendry@student.pradita.ac.id>
2025-03-31 14:25:08 +0700

    fix for script

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,11 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @env('testing')
+            {{-- skip vite during testing --}}
+        @else
+            @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @endenv
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
