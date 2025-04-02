acd113fe8a448b6338336b9ba6a02d268db1021c
Frrama <arjunafadli@gmail.com>
2025-04-02 09:27:50 +0700

    update Controller Jenjang

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\JenjangdController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 use App\Http\Middleware\AdminMiddleware;
