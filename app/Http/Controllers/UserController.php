aff9a1b6e5c17cb5f7c86a25871c3e24ce9b2684
Frrama <arjunafadli@gmail.com>
2025-04-02 19:59:18 +0700

    update controller users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use App\Models\Role;
+use App\Models\Jenjang;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
