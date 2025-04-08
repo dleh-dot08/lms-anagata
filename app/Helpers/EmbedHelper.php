91347f55be51d8f5d8bab59a4ce8c2d487e4dd51
Frrama <arjunafadli@gmail.com>
2025-04-08 20:52:15 +0700

    update

--- app/Helpers/EmbedHelper.php
+++ app/Helpers/EmbedHelper.php
@@ -3,7 +3,7 @@
 if (!function_exists('convertToEmbed')) {
     function convertToEmbed($url)
     {
-        // Convert YouTube
+        // YouTube
         if (strpos($url, 'youtube.com') !== false || strpos($url, 'youtu.be') !== false) {
             if (strpos($url, 'watch?v=') !== false) {
                 return str_replace('watch?v=', 'embed/', $url);
@@ -13,14 +13,13 @@ if (!function_exists('convertToEmbed')) {
             }
         }
 
-        // Convert Google Drive
+        // Google Drive
         if (strpos($url, 'drive.google.com') !== false) {
             if (preg_match('/file\/d\/(.*?)\//', $url, $matches)) {
                 return "https://drive.google.com/file/d/{$matches[1]}/preview";
             }
         }
 
-        // Default return
         return $url;
     }
 }
