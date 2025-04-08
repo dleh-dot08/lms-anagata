439622b9178f168954352747465f2eb7dd502685
Frrama <arjunafadli@gmail.com>
2025-04-08 21:00:33 +0700

    update

--- app/Helpers/EmbedHelper.php
+++ app/Helpers/EmbedHelper.php
@@ -3,23 +3,27 @@
 if (!function_exists('convertToEmbed')) {
     function convertToEmbed($url)
     {
-        // YouTube
-        if (strpos($url, 'youtube.com') !== false || strpos($url, 'youtu.be') !== false) {
-            if (strpos($url, 'watch?v=') !== false) {
-                return str_replace('watch?v=', 'embed/', $url);
-            } elseif (strpos($url, 'youtu.be') !== false) {
-                $videoId = basename($url);
-                return "https://www.youtube.com/embed/{$videoId}";
-            }
+        if (str_contains($url, 'youtube.com')) {
+            return preg_replace(
+                '/watch\?v=([a-zA-Z0-9_-]+)/',
+                'embed/$1',
+                $url
+            );
         }
 
-        // Google Drive
-        if (strpos($url, 'drive.google.com') !== false) {
-            if (preg_match('/file\/d\/(.*?)\//', $url, $matches)) {
-                return "https://drive.google.com/file/d/{$matches[1]}/preview";
+        if (str_contains($url, 'drive.google.com')) {
+            if (preg_match('/\/file\/d\/(.*?)\//', $url, $matches)) {
+                return 'https://drive.google.com/file/d/' . $matches[1] . '/preview';
             }
         }
 
         return $url;
+    }
+}
+
+if (!function_exists('convertToPreview')) {
+    function convertToPreview($url)
+    {
+        return convertToEmbed($url); // asumsikan sama, bisa dipisah jika logika beda
     }
 }
