b4b396a21793564785bcaa8719e87c2cd4b990ee
Frrama <arjunafadli@gmail.com>
2025-04-08 20:48:15 +0700

    update composer.json , helper embed

---
+++ app/Helpers/EmbedHelper.php
@@ -0,0 +1,26 @@
+<?php
+
+if (!function_exists('convertToEmbed')) {
+    function convertToEmbed($url)
+    {
+        // Convert YouTube
+        if (strpos($url, 'youtube.com') !== false || strpos($url, 'youtu.be') !== false) {
+            if (strpos($url, 'watch?v=') !== false) {
+                return str_replace('watch?v=', 'embed/', $url);
+            } elseif (strpos($url, 'youtu.be') !== false) {
+                $videoId = basename($url);
+                return "https://www.youtube.com/embed/{$videoId}";
+            }
+        }
+
+        // Convert Google Drive
+        if (strpos($url, 'drive.google.com') !== false) {
+            if (preg_match('/file\/d\/(.*?)\//', $url, $matches)) {
+                return "https://drive.google.com/file/d/{$matches[1]}/preview";
+            }
+        }
+
+        // Default return
+        return $url;
+    }
+}
