5a586c9050b9c7ce2c9bd9b5af24dd000c5f42d1
Oktavianus Hendry <oktavianus.hendry@student.pradita.ac.id>
2025-04-14 13:40:08 +0700

    Enhance access control and UI improvements for biodata and course management

--- resources/views/layouts/peserta/sidebar.blade.php
+++ resources/views/layouts/peserta/sidebar.blade.php
@@ -1,6 +1,6 @@
 <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
     <div class="app-brand demo">
-        <a href="user-dashboard" class="app-brand-link">
+        <a href="{{ route('peserta.dashboard') }}" class="app-brand-link">
             <span class="app-brand-logo demo">
                 <div class="sidebar-header">
                     <img src="{{ asset('assets/img/illustrations/logo-asn.png') }}" alt="logo-asn" class="logo-image">
@@ -10,44 +10,53 @@
             <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
                 <i class="bx bx-chevron-left bx-sm align-middle"></i>
             </a>
+        </a>
     </div>
 
     <div class="menu-inner-shadow"></div>
 
     <ul class="menu-inner py-1">
         <!-- Dashboard -->
-        <li class="menu-item active">
+        <li class="menu-item {{ Request::is('peserta/dashboard') ? 'active' : '' }}">
             <a href="{{ route('peserta.dashboard') }}" class="menu-link">
                 <i class="menu-icon tf-icons bx bx-home-circle"></i>
                 <div data-i18n="Analytics">Dashboard</div>
             </a>
         </li>
-        <li class="menu-item">
+
+        <!-- Biodata -->
+        <li class="menu-item {{ Request::is('biodata*') ? 'active' : '' }}">
             <a href="{{ route('biodata.index') }}" class="menu-link">
                 <i class="menu-icon tf-icons bx bxs-user-account"></i>
                 <div data-i18n="Analytics">Biodata</div>
             </a>
         </li>
-        <li class="menu-item">
+
+
+        <!-- Kursus -->
+        <li class="menu-item {{ Request::is('peserta/kursus*') ? 'active' : '' }}">
             <a href="{{ route('courses.indexpeserta') }}" class="menu-link">
                 <i class="menu-icon tf-icons bx bxs-book-alt"></i>
                 <div data-i18n="Analytics">Kursus</div>
             </a>
         </li>
-        <li class="menu-item">
+
+        <!-- Absensi -->
+        <li class="menu-item {{ Request::is('courses/absensi') ? 'active' : '' }}">
             <a href="{{ route('courses.index') }}" class="menu-link">
-                <i class="menu-icon tf-icons bx bx bxs-calendar-check"></i>
+                <i class="menu-icon tf-icons bx bxs-calendar-check"></i>
                 <div data-i18n="Analytics">Absensi</div>
             </a>
         </li>
-        <li class="menu-item">
+
+        <!-- Sertifikat -->
+        <li class="menu-item {{ Request::is('courses/sertifikat') ? 'active' : '' }}">
             <a href="{{ route('courses.index') }}" class="menu-link">
                 <i class="menu-icon tf-icons bx bxs-badge-check"></i>
                 <div data-i18n="Analytics">Sertifikat</div>
             </a>
         </li>
     </ul>
-
 </aside>
 
 <style>
@@ -62,10 +71,7 @@
 
     .logo-image {
         max-width: 100%;
-        /* Ensure it doesn't overflow the container */
         width: 80%;
-        /* Adjust this value as needed */
         height: auto;
-        /* Maintain the aspect ratio */
     }
 </style>
