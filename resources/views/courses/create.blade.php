f28c6090678997604f518c9e2f800f67195618b9
Frrama <arjunafadli@gmail.com>
2025-04-24 14:07:12 +0700

    Update comments for jQuery and Select2 script loading in course creation form for improved clarity

--- resources/views/courses/create.blade.php
+++ resources/views/courses/create.blade.php
@@ -80,11 +80,11 @@
 @endsection
 
 @push('scripts')
-    <!-- Load jQuery -->
+    <!-- Pastikan jQuery di-load terlebih dahulu -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
-    <!-- Load Select2 -->
-    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+    <!-- Kemudian Select2 -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js"></script>
 
     <!-- Script untuk Select2 -->
     <script>
