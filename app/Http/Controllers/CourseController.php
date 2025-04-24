753ebc10d881fa98e7c960ff6f4476c67e9de8cc
Frrama <arjunafadli@gmail.com>
2025-04-24 15:30:53 +0700

    Add missing Carbon import in CourseController for date handling

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -9,6 +9,7 @@ use App\Models\Kategori;
 use App\Models\Jenjang;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Carbon\Carbon;
 
 
 class CourseController extends Controller
