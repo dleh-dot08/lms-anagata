bf2d667b4707d7c6a4dcd36e3126b990a224ea9b
Frrama <arjunafadli@gmail.com>
2025-04-28 14:50:03 +0700

    Refactor project preview section; update headings for clarity and streamline iframe rendering logic.

--- resources/views/projects/peserta/show.blade.php
+++ resources/views/projects/peserta/show.blade.php
@@ -6,51 +6,51 @@
     <h5>Kursus: {{ $project->course->nama_kursus }}</h5>
 
     <div class="mb-3">
-        <h4>HTML</h4>
+        <h4>Preview Project</h4>
+        <iframe id="preview" style="width: 100%; height: 500px; border: 1px solid #ddd;"></iframe>
+    </div>
+
+    <div class="mb-3">
+        <h4>Kode HTML</h4>
         <pre>{{ $project->html_code }}</pre>
     </div>
 
     <div class="mb-3">
-        <h4>CSS</h4>
+        <h4>Kode CSS</h4>
         <pre>{{ $project->css_code }}</pre>
     </div>
 
     <div class="mb-3">
-        <h4>JavaScript</h4>
+        <h4>Kode JavaScript</h4>
         <pre>{{ $project->js_code }}</pre>
     </div>
-
-    <h2>Live Preview</h2>
-    <iframe id="preview" style="width: 100%; height: 500px; border: 1px solid #ddd;"></iframe>
 
     <a href="{{ route('projects.peserta.edit', $project->id) }}" class="btn btn-warning">Edit Project</a>
     <a href="{{ route('projects.peserta.index') }}" class="btn btn-secondary">Kembali ke Daftar Project</a>
 </div>
 
 <script>
-    function updatePreview() {
+    document.addEventListener('DOMContentLoaded', function () {
         var iframe = document.getElementById('preview');
-        var htmlCode = document.getElementById('html_code').value;
-        var cssCode = document.getElementById('css_code').value;
-        var jsCode = document.getElementById('js_code').value;
-
         var iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
+
+        var htmlCode = `{!! addslashes($project->html_code) !!}`;
+        var cssCode = `{!! addslashes($project->css_code) !!}`;
+        var jsCode = `{!! addslashes($project->js_code) !!}`;
+
         iframeDoc.open();
         iframeDoc.write(`
             <html>
                 <head>
-                    <link rel="stylesheet" href="https://unpkg.com/leaflet-routing-machine@3.2.12/dist/leaflet-routing-machine.css">
                     <style>${cssCode}</style>
                 </head>
                 <body>
                     ${htmlCode}
-
-                    <script src="https://unpkg.com/leaflet-routing-machine@3.2.12/dist/leaflet-routing-machine.js"><\/script>
                     <script>${jsCode}<\/script>
                 </body>
             </html>
         `);
         iframeDoc.close();
-    }
+    });
 </script>
 @endsection
