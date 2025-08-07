9ba92ac7fdcc5c1058b9d1e16617727b4437665f
Frrama <arjunafadli@gmail.com>
2025-08-07 14:46:50 +0700

    Refactor navigation bar structure for improved readability and maintainability in Cek Pendaftaran view

--- resources/views/kka-paud/cekpendaftaran.blade.php
+++ resources/views/kka-paud/cekpendaftaran.blade.php
@@ -26,58 +26,57 @@
         }
     </style>
 </head>
-<!-- ... (Bagian head dan style tetap sama) ... -->
-
 <body>
+
     <nav class="navbar navbar-expand-lg bg-white border-bottom fixed-top">
         <div class="container-fluid d-flex justify-content-between align-items-center mx-lg-5">
             <a class="navbar-brand d-flex align-items-center" href="#">
-            <img
-                src="{{ asset('koding_ka25/logo_all.png') }}"
-                alt="Anagata Academy Logo"
-            />
+                <img
+                    src="{{ asset('koding_ka25/logo_all.png') }}"
+                    alt="Anagata Academy Logo"
+                />
             </a>
 
             <div class="d-none d-lg-flex mx-auto">
-            <ul class="navbar-nav gap-4">
-                <li class="nav-item">
-                <a class="nav-link fw-semibold text-dark" href="#">Beranda</a>
-                </li>
-                <li class="nav-item">
-                <a
-                    class="nav-link fw-semibold text-dark"
-                    href="https://ruanganagata.id/faq"
-                    >FAQ</a
-                >
-                </li>
-                <li class="nav-item">
-                <a
-                    class="nav-link fw-semibold text-dark"
-                    href="https://ruanganagata.id"
-                    >LMS-RuangAnagata</a
-                >
-                </li>
-                <li class="nav-item">
-                <a class="nav-link fw-semibold text-dark" href="/kka-paud/cek-pendaftaran">
-                    Cek Status Pendaftaran
-                </a>
-                </li>
-                <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle fw-semibold text-dark" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Pembayaran
-                </a>
-                <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="#">Invoice</a></li>
-                    <li><a class="dropdown-item" href="#">Receipt</a></li>
-                    <li><hr class="dropdown-divider" /></li>
-                    <li><a class="dropdown-item" href="#">Upload Bukti Pembayaran</a></li>
+                <ul class="navbar-nav gap-4">
+                    <li class="nav-item">
+                        <a class="nav-link fw-semibold text-dark" href="#">Beranda</a>
+                    </li>
+                    <li class="nav-item">
+                        <a
+                            class="nav-link fw-semibold text-dark"
+                            href="https://ruanganagata.id/faq"
+                            >FAQ</a
+                        >
+                    </li>
+                    <li class="nav-item">
+                        <a
+                            class="nav-link fw-semibold text-dark"
+                            href="https://ruanganagata.id"
+                            >LMS-RuangAnagata</a
+                        >
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link fw-semibold text-dark" href="/kka-paud/cek-pendaftaran">
+                            Cek Status Pendaftaran
+                        </a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle fw-semibold text-dark" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Pembayaran
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Invoice</a></li>
+                            <li><a class="dropdown-item" href="#">Receipt</a></li>
+                            <li><hr class="dropdown-divider" /></li>
+                            <li><a class="dropdown-item" href="#">Upload Bukti Pembayaran</a></li>
+                        </ul>
+                    </li>
                 </ul>
-                </li>
-            </ul>
             </div>
         </div>
-        </nav>
-    <div class="container my-5">
+    </nav>
+    <div class="container my-5 pt-5">
         <div class="row justify-content-center">
             <div class="col-lg-10">
                 <div class="card shadow-lg border-0">
@@ -129,15 +128,13 @@
                                         </tr>
                                     @endif
                                 </tbody>
-                                </table>
-                            </div>
+                            </table>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
