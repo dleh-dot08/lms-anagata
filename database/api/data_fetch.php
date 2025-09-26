aeeae83cfa78070d0d90f0f1ebc1e1d6360a6d6d
Frrama <arjunafadli@gmail.com>
2025-09-26 10:41:18 +0700

    feat: implement data fetching API for user information in JSON format

---
+++ database/api/data_fetch.php
@@ -0,0 +1,31 @@
+<?php
+// Koneksi ke database
+$servername = "localhost";
+$username = "u742748395_lms_asn";
+$password = "m@ilChip08";
+$dbname = "u742748395_lms_asn";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Cek koneksi
+if ($conn->connect_error) {
+    die("Koneksi gagal: " . $conn->connect_error);
+}
+
+// Ambil data
+$sql = "SELECT id, nama, email FROM pengguna";
+$result = $conn->query($sql);
+
+$data = array();
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        $data[] = $row;
+    }
+}
+
+// Keluarkan data dalam format JSON
+header('Content-Type: application/json');
+echo json_encode($data);
+
+$conn->close();
+?>
